304709a37a8b4281d78d8fbfba0e655fc07044c8
Surendra-iOS <surendra.patel@thinkhat.ai>
2024-07-22 17:20:32 +0530

    Initial Commit

---
+++ Movie Tracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Movie Tracker
+//
+//  Created by surendra on 22/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Movie Tracker/Movie_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Movie_TrackerApp.swift
+//  Movie Tracker
+//
+//  Created by surendra on 22/07/24.
+//
+
+import SwiftUI
+
+@main
+struct Movie_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
