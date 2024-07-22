97e22b009d02cece99e7e84be459c20f9806f954
Surendra-iOS <surendra.patel@thinkhat.ai>
2024-07-22 17:30:49 +0530

    Movie Tracker

---
+++ Movie Tracker/MovieTracker.swift
@@ -0,0 +1,18 @@
+//
+//  MovieTracker.swift
+//  Movie Tracker
+//
+//  Created by surendra on 22/07/24.
+//
+
+import SwiftUI
+
+struct MovieTracker: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MovieTracker()
+}
