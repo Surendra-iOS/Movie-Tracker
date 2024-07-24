36595ce38fc8228f1fa28d9d631640ec86dc3ff7
Surendra-iOS <surendra.patel@thinkhat.ai>
2024-07-24 19:24:31 +0530

    Third Update

---
+++ Movie Tracker/Movie.swift
@@ -0,0 +1,8 @@
+//
+//  Movie.swift
+//  Movie Tracker
+//
+//  Created by surendra on 23/07/24.
+//
+
+import Foundation
---
+++ Movie Tracker/MovieList.swift
@@ -0,0 +1,18 @@
+//
+//  MovieList.swift
+//  Movie Tracker
+//
+//  Created by surendra on 23/07/24.
+//
+
+import SwiftUI
+
+struct MovieList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MovieList()
+}
